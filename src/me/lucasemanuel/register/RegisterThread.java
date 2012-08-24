ae5f9ebcccdafe8eec214998bcd9e9121de579d9
Lucas Arnström <lucasarnstrom@gmail.com>
2012-08-24 17:13:07 +0200

    i hate git
    Signed-off-by: Lucas Arnström <lucasarnstrom@gmail.com>

--- src/me/lucasemanuel/register/RegisterThread.java
+++ src/me/lucasemanuel/register/RegisterThread.java
@@ -54,26 +54,22 @@ public class RegisterThread extends Thread {
 		
 		if(answer != null) {
 			switch(answer) {
-			
-			case "0":
-				player.sendMessage(ChatColor.GREEN + "Grattis " + player.getName() + "! Du har registrerats på forumet med ranken " + ChatColor.LIGHT_PURPLE + "Lärling");
-				player.chat("/sync");
-				break;
-			
-			case "1":
-				player.sendMessage(ChatColor.RED + "Rankändringen kunde inte genomföras!");
-				break;
 				
-			case "2":
-				player.sendMessage(ChatColor.RED + player.getName() + " är en moderator eller högre. Du kan inte promota/demotaa en sådan medlem");
-				break;
-				
-			case "3":
-				player.sendMessage(ChatColor.RED + player.getName() + " finns inte!");
-				break;
-				
-			default:
-				player.sendMessage(ChatColor.RED + "Felaktigt svar från fonix skript! Ställ den jäveln till rätta!");
+				case "0":
+					player.sendMessage(ChatColor.GREEN + "Grattis " + player.getName() + "! Du har registrerats på forumet med ranken " + ChatColor.LIGHT_PURPLE + "Lärling");
+					player.chat("/sync");
+					break;
+					
+				case "1":
+					player.sendMessage(ChatColor.RED + "E-post redan registrerat!");
+					break;
+					
+				case "2":
+					player.sendMessage(ChatColor.RED + "Användarnamnet finns redan!");
+					break;
+					
+				default:
+					player.sendMessage(ChatColor.RED + "Något verkar ha gått snett! Kontakta admin/mod!");
 			}
 		}
 		else {
